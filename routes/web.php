70065cfc46c3dd7b8cbe87e48558db2b5740acfa
Muhammad Khoirul Anwarudin <dimaskechiel58@gmail.com>
2025-02-18 13:02:42 +0700

    feat: add  routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,92 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'Nama: Muhammad Khoirul Anwarudin <br>NIM: 244107023007';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/article/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
+
+// Route name
+
+Route::get('/user/profile', function () {
+    return 'profile';
+})->name('profile');
+
+// Route Group dan Route Prefixes
+
+Route::middleware(['first', 'second'])->group(function () {
+Route::get('/', function () {
+        // Uses first & second middleware...
+    });
+    Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+    });
+});
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/user', [UserController::class, 'index']);
+//     Route::get('/post', [PostController::class, 'index']);
+//     Route::get('/event', [EventController::class, 'index']);
+// });
+
+
+// Route::prefix('admin')->group(function () {
+//     Route::get('/user', [UserController::class, 'index']);
+//     Route::get('/post', [PostController::class, 'index']);
+//     Route::get('/event',
+//         [EventController::class, 'index']
+//     );
+// });
+
+
+// Redirect Routes
+
+Route::redirect('/here', '/there');
+
+// View Routes
+
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+
+
+
